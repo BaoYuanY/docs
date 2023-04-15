3ac1bd6ab6d521d78ce5c4facd942e1e96eb2b77
baoyuan <baoyuan0304@gmail.com>
2023-04-15 17:35:30 +0800

    PWA

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,18 @@
 import { defineUserConfig } from 'vuepress'
 import { defaultTheme } from '@vuepress/theme-default'
+import { pwaPlugin } from '@vuepress/plugin-pwa'
 
 
 export default defineUserConfig({
-    base: '/docs/',
     lang: 'zh-CN',
     title: 'BaoYuan',
     description: '宝源的学习笔记记录',
     head: [
         ['link', { rel: 'icon', href: '/tx.png' }],
-        ['meta', { name: 'author', href: '笔记' }],
-        ['meta', { name: 'BaoYuan', href: '笔记' }],
+        ['meta', { name: 'author', href: '宝源笔记' }],
+        ['meta', { name: 'BaoYuan', href: '宝源笔记' }],
+        ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
+        ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ],
     theme: defaultTheme({
         colorModeSwitch: true,
@@ -44,5 +46,14 @@ export default defineUserConfig({
                 link: '/PHP/',
             }
         ]
-    })
+    }),
+    plugins: [
+        pwaPlugin({
+            skipWaiting: true,
+            updatePopup: {
+                message: "有新的内容更新",
+                buttonText: "立即刷新"
+            }
+        }),
+    ],
 })
